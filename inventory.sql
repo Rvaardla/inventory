ae08a9089a8b45be5f18ddb148af942e4be5b18b
Rvaardla <rvardillasecond@gmail.com>
2023-02-03 07:18:43 +0700

    first commit

---
+++ inventory.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 26, 2023 at 01:43 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `contents` text NOT NULL,
+  `admin` varchar(20) NOT NULL,
+  `status` varchar(8) NOT NULL DEFAULT 'aktif'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sbrg_keluar`
+--
+
+CREATE TABLE `sbrg_keluar` (
+  `id` int(11) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `tgl` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `penerima` varchar(35) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sbrg_keluar`
+--
+
+INSERT INTO `sbrg_keluar` (`id`, `idx`, `tgl`, `jumlah`, `penerima`, `keterangan`) VALUES
+(16, 245, '2023-01-22', 2, 'Zubaedah', 'Membeli 2 unit Iphone 11'),
+(17, 247, '2023-01-21', 1, 'Haryanto', 'Membeli 1 unit Iphone 12 Pro Max'),
+(18, 246, '2023-01-25', 1, 'Cici yanti', 'Membeli 1 unit Oppo reno 3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sbrg_masuk`
+--
+
+CREATE TABLE `sbrg_masuk` (
+  `id` int(11) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `tgl` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sbrg_masuk`
+--
+
+INSERT INTO `sbrg_masuk` (`id`, `idx`, `tgl`, `jumlah`, `keterangan`) VALUES
+(9, 244, '2020-08-07', 600, 'kk'),
+(13, 246, '2023-01-24', 5, ''),
+(14, 245, '2023-01-15', 12, 'Pemasukkan barang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slogin`
+--
+
+CREATE TABLE `slogin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `slogin`
+--
+
+INSERT INTO `slogin` (`id`, `username`, `password`, `nickname`, `role`) VALUES
+(7, 'guest', '084e0343a0486ff05530df6c705c8bb4', 'Stock', 'stock');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sstock_brg`
+--
+
+CREATE TABLE `sstock_brg` (
+  `idx` int(11) NOT NULL,
+  `nama` varchar(55) NOT NULL,
+  `jenis` varchar(30) NOT NULL,
+  `merk` varchar(40) NOT NULL,
+  `ukuran` varchar(20) NOT NULL,
+  `stock` int(12) NOT NULL,
+  `satuan` varchar(10) NOT NULL,
+  `lokasi` varchar(55) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sstock_brg`
+--
+
+INSERT INTO `sstock_brg` (`idx`, `nama`, `jenis`, `merk`, `ukuran`, `stock`, `satuan`, `lokasi`) VALUES
+(245, 'Handphone', 'Iphone 11', 'Apple', '6,6 inci', 12, 'Unit', 'STORE  PONTIANAK'),
+(246, 'Handphone', 'Android', 'Oppo Reno 3', '160.2 x 73.3 x 7.9 m', 19, 'Unit', 'STORE  PONTIANAK'),
+(247, 'Handphone', 'Iphone 12 Pro Max', 'Apple', '6,68 inci ', 19, 'Unit', 'STORE  PONTIANAK');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sbrg_keluar`
+--
+ALTER TABLE `sbrg_keluar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sbrg_masuk`
+--
+ALTER TABLE `sbrg_masuk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `slogin`
+--
+ALTER TABLE `slogin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sstock_brg`
+--
+ALTER TABLE `sstock_brg`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `sbrg_keluar`
+--
+ALTER TABLE `sbrg_keluar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `sbrg_masuk`
+--
+ALTER TABLE `sbrg_masuk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `slogin`
+--
+ALTER TABLE `slogin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `sstock_brg`
+--
+ALTER TABLE `sstock_brg`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=248;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
